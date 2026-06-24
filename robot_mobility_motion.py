86f0177d890d33bb4102e0d963668c1f98de8143
JT Chen <jtchen9@gmail.com>
2026-06-24 09:28:24 +0800

    cd-17

--- robot_mobility_motion.py
+++ robot_mobility_motion.py
@@ -62,7 +62,7 @@ TURN_DT = 0.02
 TURN_STOP_MARGIN_DEG = 0.0
 
 # Measured stationary bias of gz
-GZ_BIAS = 0.42088
+GZ_BIAS = 0.41616
 
 # =========================================================
 # Limits / guards
